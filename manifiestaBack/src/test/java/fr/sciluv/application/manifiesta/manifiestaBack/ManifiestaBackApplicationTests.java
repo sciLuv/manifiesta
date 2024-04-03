1163020f793096eae9fb8244373d3ceb8f97756f
Rliance <65642655+sciLuv@users.noreply.github.com>
2024-04-03 22:35:43 +0200

    first commit

---
+++ manifiestaBack/src/test/java/fr/sciluv/application/manifiesta/manifiestaBack/ManifiestaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sciluv.application.manifiesta.manifiestaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManifiestaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Manifiesta
+
+npm install --save-dev webpack webpack-cli webpack-dev-server html-webpack-plugin style-loader css-loader file-loader bootstrap react-bootstrap nanoid
