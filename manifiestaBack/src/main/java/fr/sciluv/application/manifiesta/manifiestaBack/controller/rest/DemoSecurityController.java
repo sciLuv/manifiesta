213bb21a2000f6d4a21d28b9f9c0cc58a80d905e
Rliance <65642655+sciLuv@users.noreply.github.com>
2024-04-14 16:23:21 +0200

    adding settings in few files for security and it database

---
+++ manifiestaBack/src/main/java/fr/sciluv/application/manifiesta/manifiestaBack/controller/rest/DemoSecurityController.java
@@ -0,0 +1,23 @@
+package fr.sciluv.application.manifiesta.manifiestaBack.controller.rest;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoSecurityController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('client_user')")
+    public String hello(){
+        return "hello fromSpring Boot Keycloak (Public)";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2(){
+        return "hello fromSpring Boot Keycloak (ADMIN)";
+    }
+}
